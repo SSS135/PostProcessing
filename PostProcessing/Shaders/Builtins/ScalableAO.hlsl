5baecba7c94786dfb33c1983635c1bca28348583
Alexander Penkin <sss13594@gmail.com>
2023-11-01 14:48:50 +0500

    fix ambiend acclusion

--- PostProcessing/Shaders/Builtins/ScalableAO.hlsl
+++ PostProcessing/Shaders/Builtins/ScalableAO.hlsl
@@ -384,7 +384,7 @@ float4 FragComposition(VaryingsDefault i) : SV_Target
     float2 delta = _SAOcclusionTexture_TexelSize.xy / DOWNSAMPLE;
     half ao = BlurSmall(TEXTURE2D_PARAM(_SAOcclusionTexture, sampler_SAOcclusionTexture), i.texcoord, delta);
     ao = EncodeAO(ao);
-    return float4(ao * _AOColor, ao);
+    return float4(ao * _AOColor, 0);
 }
 
 #if !SHADER_API_GLES // Excluding the MRT pass under GLES2
